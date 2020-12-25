e41e0483bf756a7132b8d48bc0e1381d50989412
Alquipo <alquiponeto@outlook.com.br>
2020-12-25 19:16:55 -0300

    chore: sectionHero FrontEnd

--- Web/src/types/api.ts
+++ Web/src/types/api.ts
@@ -3,6 +3,22 @@ export type LogoPros = {
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+
+  button: {
+    label: string
+    url: string
+  }
+
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoPros
+  header: HeaderProps
 }
