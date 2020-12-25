a18bfe7073db6329c1c548a0a12792e97229312f
Alquipo <alquiponeto@outlook.com.br>
2020-12-25 18:54:20 -0300

    chore: types ghraphql

--- Web/src/components/Logo/index.tsx
+++ Web/src/components/Logo/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { LogoPros } from 'types/api'
 import * as S from './styles'
 
-const Logo = () => <S.LogoWrapper src="/img/logo.svg" alt="React Avançado" />
+const Logo = ({ url, alternativeText }: LogoPros) => (
+  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+)
 
 export default Logo
---
+++ Web/src/types/api.ts
@@ -0,0 +1,8 @@
+export type LogoPros = {
+  alternativeText: string
+  url: string
+}
+
+export type LandingPageProps = {
+  logo: LogoPros
+}
