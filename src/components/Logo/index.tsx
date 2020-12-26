0ba979289bdf184b8eecdce5be814b790df701ab
Alquipo <alquiponeto@outlook.com.br>
2020-12-26 15:36:18 -0300

    fix: error import logoProps

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { LogoPros } from 'types/api'
+import { LogoProps } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
-const Logo = ({ url, alternativeText }: LogoPros) => (
+const Logo = ({ url, alternativeText }: LogoProps) => (
   <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
