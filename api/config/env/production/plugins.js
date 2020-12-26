21a9dfffea4319aadb6152933fd45aa01519d182
Alquipo <alquiponeto@outlook.com.br>
2020-12-26 06:21:21 -0300

    fix: AWS S3

--- api/config/env/production/plugins.js
+++ api/config/env/production/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: 'us-east-2',
+      region: env("AWS_REGION"),
       params: {
-        Bucket: 'alquipo-reactavancado-assets',
+        Bucket: env("AWS_BUCKET"),
       },
     },
   },
